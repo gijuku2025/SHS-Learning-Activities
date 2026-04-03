37f7baebc884df140595a5f75a4766487cfd1532
gijuku2025 <chetty-jiovaan@gijuku.ac.jp>
2026-04-03 13:00:23 +0900

    Update script.js

--- public studies/croscurricularspacedrep/script.js
+++ public studies/croscurricularspacedrep/script.js
@@ -1,5 +1,5 @@
 const app = document.getElementById("app");
-const SUBJECT = "public studies"; // change to "geometry" or "civics" in other copies
+const SUBJECT = "cross curricular"; // change to "geometry" or "civics" in other copies
 const SUBJECT_LABEL = SUBJECT.charAt(0).toUpperCase() + SUBJECT.slice(1);
 
 const CHAPTER_FILES = [
